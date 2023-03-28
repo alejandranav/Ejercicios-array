497ae17adbb5ec6ca9ed85ecf8142da18b9270b1
AENN <alejandra.nuneznavarro@gmail.com>
2023-03-27 23:19:42 -0600

    se realizó el 3er ejercicio y se modificaron elementos del 1ro

--- Arreglos Ejercicios/js/main.js
+++ Arreglos Ejercicios/js/main.js
@@ -1,8 +1,8 @@
 
 console.log("******* EJERCICIO1 ********");
 // Part 1 Array 3 people
+let acomodo=""
 let fila= ["Sofia", "David", "Juan"];
-let divAlert=document.getElementById("divAlert");
 //Part 2 add 2 people, the first one is out
 fila.push("Sara");
 fila.push("Agustín");
@@ -18,7 +18,10 @@ divAlert.innerHTML+= "<ol><li>" + fila[0]
 +"<li>" + fila[4] + "</li>"
 +"<li>" + fila[5] + "</li>"
 +"<li>" + fila[6] + "</li></ol>";
-console.log(fila);
+console.log(fila.length);
+console.log(acomodo+fila);
+
+
 
 
 console.log("******* EJERCICIO2 ********");
@@ -28,96 +31,33 @@ for (let cont = "*"; cont<"******"; cont+="*") {
     console.log(cont); 
 }
 
+
+
+
 console.log("******* EJERCICIO3 ********");
-
-
-/*Write while loops to do the following:
-
-– Repeatedly print the value of the variable xValue, decreasing it by 0.5 each time,
-as long as xValue remains positive.
-- Print all the odd numbers between 1 - 100.
-- Write a method with a while loop to print 1 through n in square brackets. 
-For example, if n = 6 print [1] [2] [3] [4] [5] [6]
-- Write a method with a while loop that computes the sum of first n positive integers: 
-sum = 1 + 2 + 3 + … + n
-Examples:
-n = 5 sum = 15
-n = 19 sum = 190*/
-
-
-
-
-
-
-
-
-
-
-/*divAlert.innerHTML+= "<ol><li>" + libros[0] 
-+ "</li>"+"<li>" + libros[1] + "</li>"
-+"<li>" + libros[2] + "</li>"
-+"<li>" + libros[3] + "</li>"
-+"<li>" + libros[4] + "</li>"
-+"<li>" + libros[5] + "</li>"
-+"<li>" + libros[6] + "</li>"
-+"<li>" + libros[7] + "</li></ol>";*/
-/*divAlert.innerHTML+="<li>" + libros[1] + "</li>"
-divAlert.innerHTML+="<li>" + libros[2] + "</li>"
-divAlert.innerHTML+="<li>" + libros[3] + "</li>"
-divAlert.innerHTML+="<li>" + libros[4] + "</li>"
-divAlert.innerHTML+="<li>" + libros[5] + "</li>"
-divAlert.innerHTML+="<li>" + libros[6] + "</li>"
-divAlert.innerHTML+="<li>" + libros[7] + "</li>" +"</ol>";*/
-/*let nums=[1, 30, 4, 21, 100000]
-console.log(nums);
-console.log(nums.sort());
-console.log(nums.sort(compareNumbers));
-console.log(libros.join(", ").toUpperCase)
-let strLibros= libros.join(", ");
-console.log (strLibros)
-let arregloLibros= strLibros.split(", ")
-console.log(arregloLibros.length);
-function  compareNumbers
-(a,b) {return a-b}*/
-
-/*console.log("*************************** FOR *******************");
-for (let cont = 0; cont<100; cont+=5) {
-    console.log(cont); 
-}
-console.log("*************************** WHILE *******************");
-cont=0;
-while (cont<100){
-    console.log(cont);
-    cont+=5;
-}
-
-console.log("*************************** DO WHILE *******************");
-cont=0;
+console.log("decreasing 0.5")
+//decrease 0.5 
+let decr=100
 do {
-    console.log(cont);
-    cont+=5;
-} while (cont<100);*/
+    console.log(decr);
+    decr-=.5;
+} while (decr>0);
 
-/*console.log("***** Libros ********")
 
-for (let index=0; index<libros.length; index++){
-    console.log(libros[index])
-}
-console.log("******* While ********")
-index=0
-while (index<libros.length) {
-    console.log(libros[index]);
-    index++;
+console.log("brakets")
+let varios= ("")
+let nu=0;
+while (nu<6) {
+    nu ++;
+    console.log("["+nu+"]");
 }
 
-console.log("********* Do *********")
-index=0
-do {
-    console.log(libros[index]);
-    index++;
-} while (index<libros.length);
 
-console.log("********* Reverse *********")
-for (index=libros.length-1;index>=0; index--){
-    console.log(libros[index])}*/
-
+console.log("suma de n+x")
+//loop suma n
+let n = 0;
+let x = 0;
+while (n < 20) {
+  n ++;
+  console.log(x += n);
+};
